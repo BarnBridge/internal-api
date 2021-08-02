53cb44cc22203ab69994925d58d53535f63a8bcb
Alex T <alex@tzapu.com>
2021-08-02 15:05:17 +0300

    updated names

--- smartyield/types/pool.go
+++ smartyield/types/pool.go
@@ -1,9 +1,5 @@
 package types
 
-import (
-	"github.com/shopspring/decimal"
-)
-
 type Pool struct {
 	ProtocolId         string `json:"protocolId"`
 	ControllerAddress  string `json:"controllerAddress"`
@@ -20,21 +16,4 @@ type Pool struct {
 	RewardPoolAddress  string `json:"rewardPoolAddress"`
 
 	State PoolState `json:"state"`
-}
-
-type PoolState struct {
-	BlockNumber           int64           `json:"blockNumber"`
-	BlockTimestamp        uint64          `json:"blockTimestamp"`
-	SeniorLiquidity       decimal.Decimal `json:"seniorLiquidity"`
-	JuniorLiquidity       decimal.Decimal `json:"juniorLiquidity"`
-	JTokenPrice           decimal.Decimal `json:"jTokenPrice"`
-	SeniorAPY             float64         `json:"seniorApy"`
-	JuniorAPY             float64         `json:"juniorApy"`
-	OriginatorApy         float64         `json:"originatorApy"`
-	OriginatorNetApy      float64         `json:"originatorNetApy"`
-	AvgSeniorMaturityDays float64         `json:"avgSeniorMaturityDays"`
-	NumberOfSeniors       int64           `json:"numberOfSeniors"`
-	NumberOfJuniors       int64           `json:"numberOfJuniors"`
-	NumberOfJuniorsLocked int64           `json:"numberOfJuniorsLocked"`
-	JuniorLiquidityLocked decimal.Decimal `json:"juniorLiquidityLocked"`
 }
---
+++ smartyield/types/pool_state.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+type PoolState struct {
+	BlockNumber           int64           `json:"blockNumber"`
+	BlockTimestamp        uint64          `json:"blockTimestamp"`
+	SeniorLiquidity       decimal.Decimal `json:"seniorLiquidity"`
+	JuniorLiquidity       decimal.Decimal `json:"juniorLiquidity"`
+	JTokenPrice           decimal.Decimal `json:"jTokenPrice"`
+	SeniorAPY             float64         `json:"seniorApy"`
+	JuniorAPY             float64         `json:"juniorApy"`
+	OriginatorApy         float64         `json:"originatorApy"`
+	OriginatorNetApy      float64         `json:"originatorNetApy"`
+	AvgSeniorMaturityDays float64         `json:"avgSeniorMaturityDays"`
+	NumberOfSeniors       int64           `json:"numberOfSeniors"`
+	NumberOfJuniors       int64           `json:"numberOfJuniors"`
+	NumberOfJuniorsLocked int64           `json:"numberOfJuniorsLocked"`
+	JuniorLiquidityLocked decimal.Decimal `json:"juniorLiquidityLocked"`
+}
