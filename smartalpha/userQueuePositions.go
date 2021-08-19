33eb7902d494ca4ec734456486fe861f00af6cd4
Bogdan Lacatusu <ionut.bogdy00@gmail.com>
2021-08-19 14:43:38 +0300

    WIP:add user queue position endpoint

--- smartalpha/userQueuePositions.go
+++ smartalpha/userQueuePositions.go
@@ -2,8 +2,16 @@ package smartalpha
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+
+	"github.com/barnbridge/internal-api/response"
+	"github.com/barnbridge/internal-api/utils"
 )
 
 func (s *SmartAlpha) UserQueuePositions(ctx *gin.Context) {
-
+	userAddress := ctx.Param("address")
+	userAddress, err := utils.ValidateAccount(userAddress)
+	if err != nil {
+		response.Error(ctx, errors.Wrap(err, "invalid user address"))
+	}
 }
