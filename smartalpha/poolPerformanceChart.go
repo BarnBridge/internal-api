b4338b6389fbe4ebb4374b3e0473d038fcd99f62
Bogdan Lacatusu <ionut.bogdy00@gmail.com>
2021-09-21 13:59:56 +0300

    Refactor code

--- smartalpha/poolPerformanceChart.go
+++ smartalpha/poolPerformanceChart.go
@@ -1,13 +1,12 @@
 package smartalpha
 
 import (
-	"context"
 	"database/sql"
-	"fmt"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 	"github.com/pkg/errors"
 
 	"github.com/barnbridge/internal-api/response"
@@ -17,49 +16,62 @@ import (
 
 func (s *SmartAlpha) poolPerformanceChart(ctx *gin.Context) {
 	poolAddress := ctx.Param("poolAddress")
-	if poolAddress != "" {
-		poolAddress, err := utils.ValidateAccount(poolAddress)
-
-		if err != nil {
-			response.Error(ctx, err)
-			return
-		}
-		err, exists := s.checkPoolExists(ctx, poolAddress)
-		if err != nil {
-			response.Error(ctx, err)
-			return
-		}
-
-		if !exists {
-			response.NotFound(ctx)
-			return
-		}
+	if poolAddress == "" {
+		response.BadRequest(ctx, errors.New("invalid pool address"))
+		return
 	}
 
-	window := strings.ToLower(ctx.DefaultQuery("window", "30d"))
-	startTs, endTs, totalPoints, err := s.poolPerformanceWindow(ctx, window, poolAddress)
+	var epoch1Start, epochDuration, currentEpoch int64
+
+	poolAddress, err := utils.ValidateAccount(poolAddress)
 	if err != nil {
 		response.Error(ctx, err)
 		return
 	}
 
-	query := fmt.Sprintf(`
-				select to_timestamp(ts)                                              as point,
-					   coalesce(senior_without_sa, 0)                                as senior_without_sa,
-					   coalesce(senior_with_sa, 0)                                   as senior_with_sa,
-					   coalesce(junior_without_sa, 0)                                as junior_without_sa,
-					   coalesce(junior_with_sa, 0)                                   as junior_with_sa,
-					   coalesce(token_price_at_ts((select pool_token_address
-												   from smart_alpha.pools
-												   where pool_address = $3),
-												  (select oracle_asset_symbol
-												   from smart_alpha.pools
-												   where pool_address = $3), ts), 0) as pool_token_price
-				from generate_series($1,$2, %s) as ts
-						 inner join smart_alpha.performance_at_ts($3, ts) on true
-				order by ts;`, totalPoints)
+	err = s.db.Connection().QueryRow(ctx, `
+				select p.epoch1_start,
+					   p.epoch_duration
+				from smart_alpha.pools p
+				where p.pool_address = $1`, poolAddress).Scan(&epoch1Start, &epochDuration)
+	if err != nil && err != pgx.ErrNoRows {
+		response.Error(ctx, err)
+		return
+	}
 
-	rows, err := s.db.Connection().Query(ctx, query, startTs, endTs, poolAddress)
+	if err == pgx.ErrNoRows {
+		response.NotFound(ctx)
+		return
+	}
+
+	currentEpoch = getCurrentEpoch(epoch1Start, epochDuration)
+	window := strings.ToLower(ctx.DefaultQuery("window", "30d"))
+	if window == "last" && currentEpoch == 0 {
+		response.OK(ctx, []types.PerformancePoint{})
+		return
+	}
+
+	startTs, endTs, err := s.poolPerformanceWindow(window, epoch1Start, epochDuration, currentEpoch)
+	if err != nil {
+		response.Error(ctx, err)
+		return
+	}
+
+	pointDistance := (endTs - startTs) / ChartNrOfPoints
+
+	query := `	select to_timestamp(ts)               as point,
+					   coalesce(senior_without_sa, 0) as senior_without_sa,
+					   coalesce(senior_with_sa, 0)    as senior_with_sa,
+					   coalesce(junior_without_sa, 0) as junior_without_sa,
+					   coalesce(junior_with_sa, 0)    as junior_with_sa,
+					   coalesce(token_price_at_ts(( select pool_token_address from smart_alpha.pools where pool_address = $4 ),
+												  ( select oracle_asset_symbol from smart_alpha.pools where pool_address = $4 ), ts),
+								0)                    as pool_token_price
+				from generate_series($1::bigint, $2::bigint, $3::bigint) as ts
+						 inner join smart_alpha.performance_at_ts($4, ts) on true
+				order by ts;`
+
+	rows, err := s.db.Connection().Query(ctx, query, startTs, endTs, pointDistance, poolAddress)
 	if err != nil && err != sql.ErrNoRows {
 		response.Error(ctx, err)
 		return
@@ -80,7 +92,7 @@ func (s *SmartAlpha) poolPerformanceChart(ctx *gin.Context) {
 	response.OK(ctx, points)
 }
 
-func (s *SmartAlpha) poolPerformanceWindow(ctx context.Context, window string, poolAddress string) (int64, int64, string, error) {
+func (s *SmartAlpha) poolPerformanceWindow(window string, epoch1Start int64, epochDuration int64, currentEpoch int64) (int64, int64, error) {
 	var startTs, endTs int64
 	var duration time.Duration
 
@@ -89,33 +101,33 @@ func (s *SmartAlpha) poolPerformanceWindow(ctx context.Context, window string, p
 		duration = 7 * 24 * time.Hour
 		startTs = time.Now().Add(-duration).Unix()
 		endTs = time.Now().Unix()
-		return startTs, endTs, "3*60*60", nil
+
+		return startTs, endTs, nil
 	case "30d":
 		duration = 30 * 24 * time.Hour
 		startTs = time.Now().Add(-duration).Unix()
 		endTs = time.Now().Unix()
-		return startTs, endTs, "12*60*60", nil
+
+		return startTs, endTs, nil
 	case "current":
-		err := s.db.Connection().QueryRow(ctx, `select start_ts,end_ts from smart_alpha.get_epoch_ts($1,(select p.epoch_id from smart_alpha.pool_epoch_info p where p.pool_address = $1 order by p.epoch_id desc limit 1))`, poolAddress).Scan(&startTs, &endTs)
-		if err != nil {
-			return 0, 0, "", errors.Wrap(err, "could not get current epoch timestamps")
-		}
-		return startTs, endTs, "3*60*60", nil
+		startTs = epoch1Start + (currentEpoch-1)*epochDuration
+		endTs = epoch1Start + currentEpoch*epochDuration - 1
+
+		return startTs, endTs, nil
 	case "last":
-		err := s.db.Connection().QueryRow(ctx, `select start_ts,end_ts  from smart_alpha.get_epoch_ts($1,((select p.epoch_id from smart_alpha.pool_epoch_info p where p.pool_address = $1 order by p.epoch_id desc limit 1)-1))`, poolAddress).Scan(&startTs, &endTs)
-		if err != nil {
-			return 0, 0, "", errors.Wrap(err, "could not get last epoch timestamps")
-		}
-		return startTs, endTs, "3*60*60", nil
+		startTs = epoch1Start + (currentEpoch-2)*epochDuration
+		endTs = epoch1Start + (currentEpoch-1)*epochDuration - 1
+
+		return startTs, endTs, nil
 	default:
 		var err error
 		duration, err = time.ParseDuration(window)
 		startTs = time.Now().Add(-duration).Unix()
 		endTs = time.Now().Unix()
 		if err != nil {
-			return 0, 0, "", errors.Wrap(err, "invalid window")
+			return 0, 0, errors.Wrap(err, "invalid window")
 		}
 
-		return startTs, endTs, "30*60", nil
+		return startTs, endTs, nil
 	}
 }
