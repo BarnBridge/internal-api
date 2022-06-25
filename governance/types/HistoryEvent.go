2b24e9e0ba73f16bec05bda24ebe8b8e031e65ef
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-25 18:40:02 +0800

    Delete governance directory

--- governance/types/HistoryEvent.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type HistoryEvent struct {
-	Name    string `json:"name"`
-	StartTs int64  `json:"startTimestamp"`
-	EndTs   int64  `json:"endTimestamp"`
-	TxHash  string `json:"txHash"`
-}
